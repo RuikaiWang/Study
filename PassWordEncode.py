254dcbf1ddbc32c80da60635b83ba3b89e3812f8
RuikaiWang <45323527+RuikaiWang@users.noreply.github.com>
2020-10-30 03:41:35 +0800

    密码加密文件
    对你的密码进行加密后再用加密的密码使用那个脚本

---
+++ PassWordEncode.py
@@ -0,0 +1,31 @@
+from Crypto.Cipher import AES
+import base64
+
+
+class EncryptDate:
+    def __init__(self):
+        self.key = "sanjinxianfengya".encode("utf8")  # 初始化密钥
+        self.length = AES.block_size  # 初始化数据块大小
+        self.aes = AES.new(self.key, AES.MODE_ECB)  # 初始化AES,ECB模式的实例
+        self.unpad = lambda date: date[0:-ord(date[-1])]
+
+    def pad(self, text):
+        count = len(text.encode('utf-8'))
+        add = self.length - (count % self.length)
+        entext = text + (chr(add) * add)
+        return entext
+
+    def encrypt(self, encrData):  # 加密函数
+        res = self.aes.encrypt(self.pad(encrData).encode("utf8"))
+        msg = str(base64.b64encode(res), encoding="utf8")
+        return msg
+
+    def decrypt(self, decrData):  # 解密函数
+        res = base64.decodebytes(decrData.encode("utf8"))
+        msg = self.aes.decrypt(res).decode("utf8")
+        return self.unpad(msg)
+
+
+eg = EncryptDate()
+res = eg.encrypt("你的密码")
+print("加密的密码:" + res)
